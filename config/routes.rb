f7b0b1dc1cde78e78b1b6f49386a0383da10cd6a
wubang <wolfwubang@gmail.com>
2015-01-10 19:39:40 +0800

    finish product

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,12 @@
 Rails.application.routes.draw do
+  resources :products
+
+  # resources :products
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
-
+  # root 'welcome#index'  resources :admins
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
